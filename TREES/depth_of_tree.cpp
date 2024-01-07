086c7370728fb4c909c3af3d8a00b2218362880b
imkhushboo <vkhushboo540@gmail.com>
2024-01-07 14:27:11 +0530

    Depth of a Binary Tree

---
+++ TREES/depth_of_tree.cpp
@@ -0,0 +1,19 @@
+/*
+  find maximum depth of a binary tree
+  problem - link : https://leetcode.com/problems/maximum-depth-of-binary-tree/
+*/
+
+class Solution
+{
+public:
+    int maxDepth(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+
+        return 1 + max(left, right);
+    }
+};
